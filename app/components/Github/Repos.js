ac8bc62fb94345cbbc7e1df1a71eeef73545a61d
Burris19 <julianhernandez19@gmail.com>
2016-10-05 16:10:11 +0000

    [add] configure components

---
+++ app/components/Github/Repos.js
@@ -0,0 +1,11 @@
+var React = require('react');
+
+var Repos = React.createClass({
+  render: function(){
+    return (
+      <div> Reposs </div>
+    )
+  }
+});
+
+module.exports = Repos;
---
+++ app/components/Github/UserProfile.js
@@ -0,0 +1,11 @@
+var React = require('react');
+
+var UserProfiles = React.createClass({
+  render: function(){
+    return (
+      <div> UserProfiles </div>
+    )
+  }
+});
+
+module.exports = UserProfiles;
